c74f5167a98a2d6d0ea01a769a4d0e87fe562786
Zach Stybel <zstybel@gmail.com>
2025-06-18 13:31:45 -0400

    Add get_summary tool stub

---
+++ tools/summarizer_tool.py
@@ -0,0 +1,13 @@
+def get_summary(notes :str) -> str:
+    """Summarizes the notes into simpler and more concise terms.
+
+    Args:
+        notes (str) : A text that could be from a slide, an article, a document, or study notes.
+
+    Returns:
+        str: A bulleted string containing a summarized and simplified version of the text
+
+    """
+
+    print (f"---Tool: get_summary called for notes") #Log tool execution
+    return notes
