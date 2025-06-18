748de47796fde631ffdf642253e7d820fe922bac
Zach Stybel <zstybel@gmail.com>
2025-06-18 17:32:14 -0400

    Updated quiz_agent

--- quiz_agent/agent.py
+++ quiz_agent/agent.py
@@ -1,8 +1,17 @@
 from google.adk.agents import Agent
+from tools.quiz_tool import generate_quiz
+
 
 root_agent = Agent(
+    name='quiz_agent_v1', 
     model='gemini-2.0-flash-001',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description="Generates practice quizes based on study notes",
+    instruction= (
+        "When given study notes or a text excerpt, call the quiz_tool tool"
+        "Produce a list of multiple choice questions with options and answers"
+        "The difficulty should vary from easy to medium to hard questions"
+        "If the tool returns an error, inform the user politely. "
+    
+    ),
+    tools = [generate_quiz],
 )
